c8c339464f782d367e5eca494a79248c13bb0a22
lbarreiro <16167237+lbarreiro@users.noreply.github.com>
2026-07-29 23:28:37 +0100

    Enhance ESPWiFiSensing with additional includes
    Added includes for cstdint and esp_wifi, and updated class structure.

--- components/esp_wifi_sensing/wifi_sensing.h
+++ components/esp_wifi_sensing/wifi_sensing.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <cstdint>
+
 #include "esphome/core/component.h"
 #include "esphome/core/log.h"
+
+#include "esp_wifi.h"
 
 namespace esphome {
 namespace esp_wifi_sensing {
@@ -11,6 +15,19 @@ class ESPWiFiSensing : public Component {
   void setup() override;
   void loop() override;
   void dump_config() override;
+
+ protected:
+  static void csi_callback_(
+      void *ctx,
+      wifi_csi_info_t *data
+  );
+
+  bool start_csi_();
+
+  volatile uint32_t csi_packet_count_{0};
+  uint32_t last_reported_count_{0};
+  uint32_t last_report_time_{0};
+  bool csi_started_{false};
 };
 
 }  // namespace esp_wifi_sensing
