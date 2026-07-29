59fa31ccbe4cd57c3fc5f6f49a52691da01b484a
lbarreiro <16167237+lbarreiro@users.noreply.github.com>
2026-07-29 23:40:23 +0100

    Add ping functionality to wifi_sensing component

--- components/esp_wifi_sensing/wifi_sensing.h
+++ components/esp_wifi_sensing/wifi_sensing.h
@@ -6,6 +6,7 @@
 #include "esphome/core/log.h"
 
 #include "esp_wifi.h"
+#include "ping/ping_sock.h"
 
 namespace esphome {
 namespace esp_wifi_sensing {
@@ -23,11 +24,17 @@ class ESPWiFiSensing : public Component {
   );
 
   bool start_csi_();
+  bool start_ping_();
 
   volatile uint32_t csi_packet_count_{0};
+
   uint32_t last_reported_count_{0};
   uint32_t last_report_time_{0};
+
   bool csi_started_{false};
+  bool ping_started_{false};
+
+  esp_ping_handle_t ping_handle_{nullptr};
 };
 
 }  // namespace esp_wifi_sensing
