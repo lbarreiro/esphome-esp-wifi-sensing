32e023aa79ab4cdbda97e9b8bba72e6783ad0e3f
lbarreiro <16167237+lbarreiro@users.noreply.github.com>
2026-08-02 16:35:29 +0100

    Fix ESP-CSI gain compensation integration

--- components/esp_wifi_sensing/gain_compensation_preprocessor.h
+++ components/esp_wifi_sensing/gain_compensation_preprocessor.h
@@ -10,13 +10,14 @@ namespace esp_wifi_sensing {
 
 class GainCompensationPreprocessor {
  public:
-  void set_enabled(bool enabled) { enabled_ = enabled; }
+  void set_enabled(bool enabled);
   bool is_enabled() const { return enabled_; }
 
   void process(const CsiPacket &input, CsiPacket &output);
 
  private:
   bool enabled_{false};
+  int last_gain_status_{-1};
   std::vector<int8_t> compensated_bytes_{};
 };
 
