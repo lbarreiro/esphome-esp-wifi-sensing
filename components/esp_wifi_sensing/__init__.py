e984169f9374907a9cb2c9771c4efdb23beffa47
lbarreiro <16167237+lbarreiro@users.noreply.github.com>
2026-08-03 10:16:07 +0100

    Fix CSI variance metric calculation

--- components/esp_wifi_sensing/__init__.py
+++ components/esp_wifi_sensing/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
+CONF_ALGORITHM = "algorithm"
 CONF_GAIN_COMPENSATION = "gain_compensation"
 
 DEPENDENCIES = ["wifi"]
@@ -13,9 +14,18 @@ ESPWiFiSensing = esp_wifi_sensing_ns.class_(
     cg.Component,
 )
 
+CsiAlgorithm = esp_wifi_sensing_ns.enum("CsiAlgorithm")
+ALGORITHMS = {
+    "absolute_sum": CsiAlgorithm.ABSOLUTE_SUM,
+    "variance": CsiAlgorithm.VARIANCE,
+}
+
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(ESPWiFiSensing),
+        cv.Optional(CONF_ALGORITHM, default="absolute_sum"): cv.enum(
+            ALGORITHMS, lower=True
+        ),
         cv.Optional(CONF_GAIN_COMPENSATION, default=False): cv.boolean,
     }
 ).extend(cv.COMPONENT_SCHEMA)
@@ -24,6 +34,7 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    cg.add(var.set_algorithm(config[CONF_ALGORITHM]))
     cg.add(var.set_gain_compensation_enabled(config[CONF_GAIN_COMPENSATION]))
     if config[CONF_GAIN_COMPENSATION]:
         cg.add_define("USE_ESP_WIFI_SENSING_GAIN_COMPENSATION")
