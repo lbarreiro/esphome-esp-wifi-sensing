b21fff701bf39887c7a780c31d2bbda4d295ba86
lbarreiro <16167237+lbarreiro@users.noreply.github.com>
2026-08-12 17:36:23 +0100

    Add adaptive motion candidate binary sensor

--- components/esp_wifi_sensing/binary_sensor.py
+++ components/esp_wifi_sensing/binary_sensor.py
@@ -6,12 +6,14 @@ from . import ESPWiFiSensing
 
 CONF_ESP_WIFI_SENSING_ID = "esp_wifi_sensing_id"
 CONF_MOTION = "motion"
+CONF_MOTION_CANDIDATE = "motion_candidate"
 CONF_TEMPORAL_PERSISTENCE = "temporal_persistence"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_ESP_WIFI_SENSING_ID): cv.use_id(ESPWiFiSensing),
         cv.Optional(CONF_MOTION): binary_sensor.binary_sensor_schema(),
+        cv.Optional(CONF_MOTION_CANDIDATE): binary_sensor.binary_sensor_schema(),
         cv.Optional(CONF_TEMPORAL_PERSISTENCE): binary_sensor.binary_sensor_schema(),
     }
 )
@@ -23,6 +25,10 @@ async def to_code(config):
     if CONF_MOTION in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_MOTION])
         cg.add(parent.set_motion_binary_sensor(sens))
+
+    if CONF_MOTION_CANDIDATE in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_MOTION_CANDIDATE])
+        cg.add(parent.set_motion_candidate_binary_sensor(sens))
 
     if CONF_TEMPORAL_PERSISTENCE in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_TEMPORAL_PERSISTENCE])
